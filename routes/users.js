f7c83c145a03b2bfcf35d7f52dcf6f54fd9933e1
Juan Daniel Martinez <mtzn_juandaniel@yahoo.com.mx>
2021-06-01 22:10:45 -0600

    Implementing the other routes (GET, POST, PUT, DELETE) for users (this doesn't work properly 'till setting up a database manager)

--- routes/users.js
+++ routes/users.js
@@ -18,6 +18,58 @@ function usersAPI(app) {
       next(err);
     }
   });
+
+  router.get('/:id', async (req, res, next) => {
+    try {
+      const user = await Promise.resolve(usersMock[0]);
+
+      res.status(200).json({
+        data: user,
+        message: 'User retrieved',
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  router.post('/', async (req, res, next) => {
+    try {
+      const createdUser = await Promise.resolve(usersMock[0].id);
+
+      res.status(201).json({
+        data: createdUser,
+        message: 'Users created',
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  router.put('/:id', async (req, res, next) => {
+    try {
+      const updatedUser = await Promise.resolve(usersMock[0]);
+
+      res.status(200).json({
+        data: updatedUser,
+        message: 'User updated',
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  router.delete('/:id', async (req, res, next) => {
+    try {
+      const deletedUser = await Promise.resolve(usersMock[0]);
+
+      res.status(200).json({
+        data: deletedUser,
+        message: 'User deleted',
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
 }
 
 module.exports = usersAPI;
