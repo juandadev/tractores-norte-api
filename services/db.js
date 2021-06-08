9d96e1e96bb5d9ccc6f0621d4c203f6a80f2043d
Juan Daniel Martinez <mtzn_juandaniel@yahoo.com.mx>
2021-06-08 13:33:26 -0600

    Showing errors in console

--- services/db.js
+++ services/db.js
@@ -2,10 +2,13 @@ const mysql = require('mysql2/promise');
 const config = require('../config');
 
 async function query(sql, params) {
-  const connection = await mysql.createConnection(config.db);
-  const [results] = await connection.execute(sql, params);
-
-  return results;
+  try {
+    const connection = await mysql.createConnection(config.db);
+    const [results] = await connection.execute(sql, params);
+    return results;
+  } catch (err) {
+    console.error('Error connecting to MySQL:', err);
+  }
 }
 
 module.exports = { query };
